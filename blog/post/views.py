285eeef587aa2cf4cdac3d4dfe1a0bb131a19b66
Rohit Tap <150374718+rohittap45@users.noreply.github.com>
2024-01-16 17:31:08 +0530

    Update views.py

--- blog/post/views.py
+++ blog/post/views.py
@@ -3,9 +3,9 @@ from . models import Post
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
-    return render(request, 'index.html', {'posts': posts})
+    return render(request, '\blog\templates\index.html', {'posts': posts})
 
 def post(request, pk):
     posts = Post.objects.get(id=pk)
-    return render(request, 'posts.html', {'posts': posts})
+    return render(request, '\blog\templates\posts.html', {'posts': posts})
 
